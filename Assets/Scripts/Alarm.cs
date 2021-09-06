6b78696d293763d0f43ab5c993b344222da66eb9
Venzele <Venzele@yandex.ru>
2021-09-06 10:35:39 +0300

    Сигнализация теперь работает по триггерам

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -4,33 +4,51 @@ public class Alarm : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
-    private WayThief _thief;
     private AudioSource _sound;
     private float _runningTime;
+    private bool _isEnterThief;
+
+    public float RunningTime
+    {
+        get
+        {
+            return _runningTime;
+        }
+        private set
+        {
+            if (value >= 1)
+                _runningTime = 1;
+            else if (value <= 0)
+                _runningTime = 0;
+            else
+                _runningTime = value;
+        }
+    }
 
     private void Start()
     {
-        _thief = FindObjectOfType<WayThief>();
         _sound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<WayThief>(out WayThief thief))
+            _isEnterThief = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<WayThief>(out WayThief thief))
+            _isEnterThief = false;
     }
 
     private void Update()
     {
-        if (transform.position.x >= _thief.transform.position.x)
-            _runningTime += Time.deltaTime * _speed;
+        if (_isEnterThief)
+            RunningTime += Time.deltaTime * _speed;
         else
-            _runningTime -= Time.deltaTime * _speed;
+            RunningTime -= Time.deltaTime * _speed;
 
-        SetMaxValue();
-
-        _sound.volume = Mathf.MoveTowards(0, 1, _runningTime);
-    }
-
-    private void SetMaxValue()
-    {
-        if (_runningTime >= 1)
-            _runningTime = 1;
-        else if (_runningTime <= 0)
-            _runningTime = 0;
+        _sound.volume = Mathf.MoveTowards(0, 1, RunningTime);
     }
 }
